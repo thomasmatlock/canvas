7f5cc62863bea3a508742b61e2535e3b77827eb3
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-04-28 19:57:49 -0400

    12 done

--- 10 drawing quadratic curves/canvas-script.js
+++ 10 drawing quadratic curves/canvas-script.js
@@ -3,12 +3,15 @@ window.onload = function() {
     let canvas = document.getElementById('hello-world-canvas');
     let context = canvas.getContext('2d');
 
-    // context.quadraticCurveTo(cpx, cpy, x, y)
+    // bezier curves have 2 control points rather than one
+    // begin by defining begin and ending points
+    // then 2 control points
+    // context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
 
     context.beginPath();
     context.strokeStyle = 'red';
     context.lineWidth = 10;
     context.moveTo(200, 250);
-    context.quadraticCurveTo(500, 10, 400, 250);
+    context.bezierCurveTo(200, 100, 100, 100, 400, 250);
     context.stroke();
 };
